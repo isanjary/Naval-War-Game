907d16e2f9fa768f726164633ed0f1ac8fae0baa
Asif Sanjary <hello@sanjar.fyi>
2025-12-15 22:00:44 +0700

    Add Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { BootScene } from './scenes/BootScene';
 import { TitleScene } from './scenes/TitleScene';
 import { GameScene } from './scenes/GameScene';
 import { UIScene } from './scenes/UIScene';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 const isDev = import.meta.env.DEV;
 
